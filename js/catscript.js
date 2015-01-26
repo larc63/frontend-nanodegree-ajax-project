f5bf06c3f1dfcd05c2030d7633bd86ef09bc3cb4
Luis Antonio <larc63@gmail.com>
2015-01-26 03:06:49 -0600

    refactored to a MVH pattern

--- js/catscript.js
+++ js/catscript.js
@@ -12,69 +12,86 @@ $(function () {
         return CAT_NAMES[i];
     };
 
-    var CatModel = {
-        name: getCatName(),
-        kittyClicks: 0,
-        _id: undefined,
-        handleClick: function () {
+    function Cat() {
+        this.name = getCatName();
+        this.kittyClicks = 0;
+        this._id = undefined;
+        this.handleClick = function () {
             this.kittyClicks++;
-            $("#kitty" + this._id + "-click-count").text(this.kittyClicks);
         }
     };
 
-    var catHerder = {
+    var catModel = {
         clowder: new Array(),
         catCount: 0,
-        listClickHandler: function (e) {
-            var theId = e.target.id;
-            var catId = parseInt(theId.substring(5, theId.length));
-            console.log("selected cat " + catId + " " + this.clowder[catId].name);
-            this.populateCatFrame(catId, "cat-frame");
-        },
-
-        clickHandler: function (e) {
-            var theId = e.target.id;
-            var catId = parseInt(theId.substring(5, theId.length - "-picture".length));
-            this.clowder[catId].handleClick();
-        },
-
-        generate: function () {
-            var kitty = new CatModel();
-            kitty._id = this.catCount;
-            kitty.name = this.getCatName();
+        add: function(kitty){
             this.catCount++;
             this.clowder.push(kitty);
         },
-        populateCatFrame: function (id, tagId) {
+        getCount: function(){
+            return this.catCount;
+        },
+        getCats: function () {
+            return this.clowder;
+        }
+    };
+    
+    var catHerder = {
+        clickHandler: function (e) {
+            var theId = e.target.id;
+            var catId = parseInt(theId.substring(5, theId.length - "-picture".length));
+            this.getCats()[catId].handleClick();
+            catFrame.render(catId);
+        },
+        listClickHandler: function (e) {
+            var theId = e.target.id;
+            var catId = parseInt(theId.substring(5, theId.length));
+            catFrame.render(catId);
+        },
+        generate: function () {
+            var kitty = new Cat();
+            kitty._id = catModel.getCount();
+            catModel.add(kitty);
+        },
+        getCats: function () {
+            return catModel.getCats();
+        }
+    };
+
+    var catList = {
+        render: function () {
+            var cats = catHerder.getCats();
+            for (var i = 0; i < cats.length; i++) {
+                var kitty = cats[i];
+                $("#clowder").append('<p id="kitty' + kitty._id + '">' + kitty.name + '</p>');
+
+                $('#kitty' + kitty._id).click(function (e) {
+                    catHerder.listClickHandler(e);
+                });
+            }
+        }
+    };
+
+    var catFrame = {
+        render: function (id) {
             var self = this;
-            var kitty = this.clowder[id];
+            var kitty = catHerder.getCats()[id];
             var retVal = '<p>' + kitty.name + '</p>';
             retVal += '<div class = "click-count" id="kitty' + kitty._id + '-click-count" > ' + kitty.kittyClicks + ' clicks</div>';
             retVal += '<div class = "kitty-picture" >';
             retVal += '<img src = "http://lorempixel.com/300/300/cats/" id = "kitty' + kitty._id + '-picture" / >';
             retVal += '</div>';
-            $("#" + tagId).empty().append(retVal);
+            $("#cat-frame").empty().append(retVal);
 
             $('#kitty' + kitty._id + '-picture').click(function (e) {
-                self.clickHandler.call(self, e);
-            });
-        },
-
-        insertList: function (id, tagId) {
-            var self = this;
-            var kitty = this.clowder[id];
-            var retVal = '<p id="kitty' + kitty._id + '">' + kitty.name + '</p>';
-            $("#" + tagId).append(retVal);
-
-            $('#kitty' + kitty._id).click(function (e) {
-                self.listClickHandler.call(self, e);
+                catHerder.clickHandler(e);
             });
         }
-    };
+    }
 
     for (var i = 0; i < 10; i++) {
         catHerder.generate();
-        catHerder.insertList(i, "clowder");
     }
-
+    catList.render();
+    catFrame.render(0);
 });
