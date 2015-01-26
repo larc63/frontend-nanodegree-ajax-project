60874efae67ac1e9498098d0c4eccad166ad4a6e
Luis Antonio <larc63@gmail.com>
2015-01-26 00:46:06 -0600

    refactored to add cat names

--- js/catscript.js
+++ js/catscript.js
@@ -1,4 +1,12 @@
+var CAT_NAMES = ["XS", "Lafeyette", "Mollye", "Tofu", "Ashes Too Ashes", "Tessa", "Asphalt", "Waffle", "Inkster", "Sydney", "Beez Louise", 
+                 "Gucci", "Chipmunk", "Dozie Darlin", "Barrin", "Senna", "Sorcerer Or Sorceress", "Netscape", "Nickedemus", "Michael Jordan", 
+                 "Bobbers", "Cinderblock", "Mollie", "Meowwow", "Heavengaze", "Karin", "Gypsyrose", "Miss Priss", "Maliboo", "Kitty Baby", 
+                 "William The Conqueror", "Winklynymph", "MacGregor", "Matador", "Bernie", "Bill The Hell Cat", "Gassy Boy", "Tinka Tinka", 
+                 "Sparkplug", "Bungle", "MacGyver", "Unit", "Ferreo", "Mlle Butterfly", "William T", "OrangEndymion", "Randall Flagg", 
+                 "Starr-O-The-West", "Cheryl"];
+
 function Cat() {
+    this.name;
     this.kittyClicks = 0;
     this._id;
 }
@@ -12,16 +20,21 @@ Cat.prototype.handleClick = function () {
 function CatHerder() {
     this.clowder = new Array();
     this.catCount = 0;
-    this.clickHandler = function(e){
+    this.clickHandler = function (e) {
         var theId = e.target.id;
         var catId = parseInt(theId.substring(5, theId.length - "-picture".length));
         this.clowder[catId].handleClick();
     };
 };
+CatHerder.prototype.getCatName = function () {
+    var i = Math.floor(Math.random()*CAT_NAMES.length);
+    return CAT_NAMES[i];
+}
 
 CatHerder.prototype.generate = function () {
     var kitty = new Cat();
     kitty._id = this.catCount;
+    kitty.name = this.getCatName();
     this.catCount++;
     this.clowder.push(kitty);
 };
@@ -29,13 +42,14 @@ CatHerder.prototype.generate = function () {
 CatHerder.prototype.insert = function (id, tagId) {
     var self = this;
     var kitty = this.clowder[id];
-    var retVal = '<div class = "click-count" id="kitty' + kitty._id + '-click-count" > ' + kitty.kittyClicks + ' </div>';
+    var retVal = '<p>' + kitty.name + '</p>';
+    retVal += '<div class = "click-count" id="kitty' + kitty._id + '-click-count" > ' + kitty.kittyClicks + ' clicks</div>';
     retVal += '<div class = "kitty-picture" >';
-    retVal += '<img src = "http://lorempixel.com/100/100/cats/" id = "kitty' + kitty._id + '-picture" / >';
+    retVal += '<img src = "http://lorempixel.com/300/300/cats/" id = "kitty' + kitty._id + '-picture" / >';
     retVal += '</div>';
     $("#" + tagId).append(retVal);
-    
-    $('#kitty' + kitty._id + '-picture').click(function(e){
+
+    $('#kitty' + kitty._id + '-picture').click(function (e) {
         self.clickHandler.call(self, e);
     });
 }
